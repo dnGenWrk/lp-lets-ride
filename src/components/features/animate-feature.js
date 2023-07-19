ac51271736ef11b10d68b601091919df3fa12e56
Dnworkf <general-n-dnmf@outlook.com>
2023-07-19 18:34:33 +0300

    update

--- src/components/features/animate-feature.js
+++ src/components/features/animate-feature.js
@@ -3,7 +3,7 @@ const animateFutureItems = function () {
   console.log("Feature: start");
   const elems = document.querySelectorAll(".lp-feature-section__card");
   if (elems) {
-    gsap.fromTo(elems, { x: -2000 }, { stagger: 0.1, x: 0, duration: 0.9, delay: 0.4, scrollTrigger: { trigger: ".section-animate" } });
+    gsap.fromTo(elems, { x: -2000 }, { stagger: 0.1, x: 0, duration: 2, delay: 0.4, ease: "back.out(1.7)", scrollTrigger: { trigger: ".section-animate" } });
   }
 };
 export default animateFutureItems;
